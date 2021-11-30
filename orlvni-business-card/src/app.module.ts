7715bb1ac4cc7897d9a637f63caa7e2ce32439ab
ORLVNIMUSIC <orlvnimusic@gmail.com>
2021-11-30 14:56:15 +0300

    Async typeorm rooting

--- orlvni-business-card/src/app.module.ts
+++ orlvni-business-card/src/app.module.ts
@@ -3,23 +3,23 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Connection } from 'typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductController } from './products/product.controller';
 import { PRODUCTS } from './products/product.entity';
 import { ProductModule } from './products/product.module';
-import { ProductService } from './products/product.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mssql',
-      host: 'localhost',
-      port: 1433,
-      username: 'user',
-      password: 'userpassword',
-      migrationsTableName: 'PRODUCTS',
-      database: 'WEB_STORE_DB',
-      entities: [PRODUCTS],
-      extra: { trustServerCertificate: true },
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'mssql',
+        host: 'localhost',
+        port: 1433,
+        username: 'user',
+        password: 'userpassword',
+        migrationsTableName: 'PRODUCTS',
+        database: 'WEB_STORE_DB',
+        entities: [PRODUCTS],
+        extra: { trustServerCertificate: true },
+      }),
     }),
     ProductModule,
   ],
