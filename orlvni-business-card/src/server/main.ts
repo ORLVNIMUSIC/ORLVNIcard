9cebc2f2a367846dfcca9c72db3a90e3988ebc3a
ORLVNIMUSIC <orlvnimusic@gmail.com>
2021-12-13 10:39:12 +0300

    minor

--- orlvni-business-card/src/server/main.ts
+++ orlvni-business-card/src/server/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 require('dotenv').config({
-  path: '../secrets.env',
+  path: './secrets.env',
 });
 import * as cookieParser from 'cookie-parser';
 
