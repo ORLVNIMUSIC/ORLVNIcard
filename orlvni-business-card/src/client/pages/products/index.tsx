133d9871cf26094c8c9a6a487983ff8494ef054f
ORLVNIMUSIC <orlvnimusic@gmail.com>
2021-12-27 12:35:53 +0300

    products page optimized

--- orlvni-business-card/src/client/pages/products/index.tsx
+++ orlvni-business-card/src/client/pages/products/index.tsx
@@ -1,22 +1,48 @@
 import Link from 'next/dist/client/link';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import MainLayout from '../../layouts/main.layout';
 
-export default function Products({
-  fetchedProducts,
-  fetchedUsers,
-  cookies,
-  host,
-}) {
-  const [data, setData] = useState({
-    products: fetchedProducts,
-    users: fetchedUsers,
-  });
+export default function Products({ cookies, host }) {
+  const [data, setData] = useState(null);
+
+  async function fetchData() {
+    const resProducts = await fetch(`${host}/server/products/available`);
+    const fetchedProducts = await resProducts.json();
+    if (!fetchedProducts) {
+      return {
+        notFound: true,
+      };
+    }
+
+    const fetchedUsers = await Promise.all(
+      fetchedProducts.map(async (el) => {
+        const responseProducts = await fetch(
+          `${host}/server/users/${el.user_id}`,
+        );
+
+        const oneUser = await responseProducts.json();
+        if (!oneUser) {
+          return {
+            notFound: true,
+          };
+        }
+        return oneUser;
+      }),
+    );
+
+    setData({
+      products: fetchedProducts,
+      users: fetchedUsers,
+    });
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   async function checkboxHandler(event) {
     event.target.disabled = true;
-    document.getElementById('AvailableLabel').innerHTML =
-      'Посмотреть только доступные (данные загружаются)';
+    setData(null);
 
     const resProducts = event.target.checked
       ? await fetch(`${host}/server/products/available`)
@@ -42,8 +68,6 @@ export default function Products({
       users: dataUsers,
     });
 
-    document.getElementById('AvailableLabel').innerHTML =
-      'Посмотреть только доступные';
     event.target.disabled = false;
   }
 
@@ -74,23 +98,29 @@ export default function Products({
           Посмотреть только доступные
         </label>
       </div>
-      {data.products.map((item) => (
-        <div className="container" key={item.product_id}>
-          <h3>{item.product_name}</h3>
-          <h4>
-            Предоставляет услугу:{' '}
-            <strong>
-              {data.users.find((el) => el.user_id === item.user_id).user_name}
-            </strong>
-          </h4>
-          <Link href={`/products/${item.product_id}`}>
-            <a>Перейти на страницу услуги</a>
-          </Link>
-          <p>{item.product_desc}</p>
-          <p>{item.product_cost} р.</p>
-          <p>{item.product_availability ? 'Доступна' : 'Недоступна'}</p>
+      {data ? (
+        data.products.map((item) => (
+          <div className="container" key={item.product_id}>
+            <h3>{item.product_name}</h3>
+            <h4>
+              Предоставляет услугу:{' '}
+              <strong>
+                {data.users.find((el) => el.user_id === item.user_id).user_name}
+              </strong>
+            </h4>
+            <Link href={`/products/${item.product_id}`}>
+              <a>Перейти на страницу услуги</a>
+            </Link>
+            <p>{item.product_desc}</p>
+            <p>{item.product_cost} р.</p>
+            <p>{item.product_availability ? 'Доступна' : 'Недоступна'}</p>
+          </div>
+        ))
+      ) : (
+        <div className="container">
+          <div className="donut" />
         </div>
-      ))}
+      )}
     </MainLayout>
   );
 }
@@ -99,31 +129,7 @@ export async function getServerSideProps(ctx) {
   const { cookies } = req;
   const host = `${process.env.PROTOCOL}://${req.rawHeaders[1]}`;
 
-  const resProducts = await fetch(`${host}/server/products/available`);
-  const fetchedProducts = await resProducts.json();
-  if (!fetchedProducts) {
-    return {
-      notFound: true,
-    };
-  }
-
-  const fetchedUsers = await Promise.all(
-    fetchedProducts.map(async (el) => {
-      const responseProducts = await fetch(
-        `${host}/server/users/${el.user_id}`,
-      );
-
-      const oneUser = await responseProducts.json();
-      if (!oneUser) {
-        return {
-          notFound: true,
-        };
-      }
-      return oneUser;
-    }),
-  );
-
   return {
-    props: { fetchedProducts, fetchedUsers, cookies, host },
+    props: { cookies, host },
   };
 }
